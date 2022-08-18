261068506af1396bc9d7fd6a4f6485bf9b8fa628
passer <z449687000@gmail.com>
2022-08-18 12:46:43 +0800

    支配树帮助类

--- tests/DominatorTreeTest.cc
+++ tests/DominatorTreeTest.cc
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <set>
+#include "ASM/CFG2DomTreeBuilder.hh"
+#include "DomTreeHelper.hh"
 #include "DominatorTree.hh"
 
 using namespace HaveFunCompiler;
@@ -51,5 +54,47 @@ TEST(DominatorTree, SimpleTest2) {
   }
   for (int i = 1; i <= 10; i++) {
     ASSERT_EQ(dominator_ans[i], sum[i]);
+  }
+}
+
+TEST(DominatorTree, DomTreeHelperTest) {
+  auto dt = std::make_shared<DominatorTree<size_t>>();
+  dt->AddEdge(1, 2);
+  dt->AddEdge(1, 3);
+  dt->AddEdge(2, 4);
+  dt->AddEdge(2, 5);
+  dt->AddEdge(3, 9);
+  dt->AddEdge(5, 6);
+  dt->AddEdge(5, 7);
+  dt->AddEdge(5, 8);
+  dt->Build(1);
+  DomTreeHelper helper(dt.get());
+  std::set<std::pair<int,int>> S;
+  auto add_positive_case = [&](int i, int j) -> void {
+    S.emplace(i, j);
+    EXPECT_TRUE(helper.IsAncestorOf(i, j)) << "(" << i << " " << j << ")";
+  };
+  for (int i = 1; i <= 9; i++) {
+    add_positive_case(1, i);
+  }
+  add_positive_case(2, 2);
+  for(int i=4;i<=8;i++){
+    add_positive_case(2, i);
+  }
+  add_positive_case(3, 3);
+  add_positive_case(3, 9);
+  add_positive_case(4, 4);
+  for (int i = 5; i <= 8; i++) {
+    add_positive_case(5, i);
+  }
+  for (int i = 6; i <= 9; i++) {
+    add_positive_case(i, i);
+  }
+  for (int i = 1; i <= 9; i++) {
+    for (int j = 1; j <= 9; j++) {
+      if (!S.count(std::make_pair(i, j))) {
+        EXPECT_FALSE(helper.IsAncestorOf(i, j)) << "(" << i << " " << j << ")";
+      }
+    }
   }
 }
