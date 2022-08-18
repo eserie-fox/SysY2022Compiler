50a20dd7c5d5b695a02f73420e6e40c9dfcf4abb
tenet <1697437561@qq.com>
2022-08-18 16:31:26 +0800

    修复没加命名空间导致链接错误

--- library/source/ASM/CFG2DomTreeBuilder.cc
+++ library/source/ASM/CFG2DomTreeBuilder.cc
@@ -1,7 +1,7 @@
 #include "ASM/CFG2DomTreeBuilder.hh"
 
-using namespace HaveFunCompiler;
-using namespace AssemblyBuilder;
+namespace HaveFunCompiler {
+namespace AssemblyBuilder {
 
 std::shared_ptr<DominatorTree<size_t>> BuildDomTreeFromCFG(std::shared_ptr<ControlFlowGraph> cfg) {
   std::shared_ptr<DominatorTree<size_t>> ret = std::make_shared<DominatorTree<size_t>>();
@@ -13,4 +13,7 @@ std::shared_ptr<DominatorTree<size_t>> BuildDomTreeFromCFG(std::shared_ptr<Contr
   }
   ret->Build(cfg->get_startNode());
   return ret;
+}
+
+}
 }
