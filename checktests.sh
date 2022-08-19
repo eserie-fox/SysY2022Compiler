66d455ea63ed707d2d36aec26921290cf87c0def
passer <z449687000@gmail.com>
2022-08-19 12:02:39 +0800

    让根目录的脚本好用一些了（checktest会检查是否有test.txt，没有会新建），删除一般没用的run脚本

--- checktests.sh
+++ checktests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+mkdir -p build
 cd build
 make Tests
-./tests/Tests $@
+cd tests
+if [ ! -f "test.txt" ]; then
+    cp ../../tests/compile_test_cases/2022_functional/26_while_test1.sy ./test.txt
+fi
+./Tests $@
